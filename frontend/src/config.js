6046979aa58a79ac53da38f6dd027f6b9fc7d5db
khangksp <129304975+khangksp@users.noreply.github.com>
2025-08-29 12:04:31 +0700

    dọn rác 3

--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-// const API_BASE_URL = "http://localhost:8000"
-// const API_BASE_URL = "http://192.168.69.142:8000";
-const API_BASE_URL = "https://a4e1f6f64b74.ngrok-free.app";
-
-export {API_BASE_URL};
